bc24437614a3c192ec6bd3b84831bb16e0c7c980
SnirTaub <Snirtaub@gmail.com>
2024-04-25 17:15:55 +0300

    added-Texts-file

---
+++ FacebookWinFormsApp/Texts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFacebookFeatures
+{
+    internal class Texts
+    {
+        internal const string Name = "Name";
+        internal const string NoAlbumsFound = "No albums were found";
+        internal const string NoPostsFound = "No posts were found";
+    }
+}
